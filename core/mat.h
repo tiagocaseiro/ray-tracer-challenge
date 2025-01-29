98ad328cd719ebe2847c082936eae28d731b4014
Tiago Ferreira <tiago_caseiro_95@hotmail.com>
2025-01-29 00:23:06 +0000

    Add new arithmetic operations and identity

--- core/mat.h
+++ core/mat.h
@@ -5,32 +5,61 @@
 #include <algorithm>
 #include <cmath>
 
+#include "common.h"
+#include "tuple.h"
+
 template<size_t n>
 struct mat
 {
-    explicit mat(const std::array<float,n*n>&);
-    std::span<float> operator[](const size_t x);
+    explicit mat(const std::array<float,n*n>& _data)
+    {
+        std::memcpy(&m_data, &_data, sizeof(decltype(_data)));
+    }
+
+    mat() = default;
+    
+    std::span<float> operator[](const size_t x)
+    {
+        auto begin = std::begin(m_data);
+        begin+=(x*n);
+        auto end = begin + n;
+        return {begin, end};
+    }
+
+    std::span<const float> operator[](const size_t x) const
+    {
+        auto begin = std::cbegin(m_data);
+        begin+=(x*n);
+        auto end = begin + n;
+        return {begin, end};    
+    }
+    
+    const std::array<float,n*n>& data() const
+    {
+        return m_data;
+    }
+    
+    static const mat<n>& identity()
+    {
+        auto init_id = []
+        {
+            mat<n> id;
+            for (int i = 0; i < n; i++)
+            {
+                id.m_data[i*n+i] = 1;
+            } 
+            return id;
+        };
+        static mat<n> id = init_id();
+        return id;
+    }
+
 private:
     std::array<float,n*n>m_data = {0};
-    
-    template<size_t n1>
-    friend std::ostream& operator<<(std::ostream& os, mat<n1> mat);
 };
 
 template<size_t n>
-mat<n>::mat(const std::array<float,n*n>& data) 
-{
-    std::memcpy(&m_data, &data, sizeof(decltype(data)));
-}
-
-template<size_t n>
-std::span<float> mat<n>::operator[](const size_t x)
-{
-    return {std::begin(m_data)+(x*n), std::begin(m_data)+(x*n+n)};
-}
-
-template<size_t n>
-std::ostream& operator<<(std::ostream& os, mat<n> mat)
+std::ostream& operator<<(std::ostream& os, const mat<n>& mat)
 {
     os << std::fixed << std::setprecision(2);
     for (int i = 0; i != n*n; i++)
@@ -44,12 +73,66 @@ std::ostream& operator<<(std::ostream& os, mat<n> mat)
             os << " ";
         }
 
-        os << mat.m_data[i];
+        os << mat.data()[i];
     }
 
     return os;
 }
 
+template<size_t n>
+bool operator==(const mat<n>& a, const mat<n>& b)
+{
+    return std::ranges::equal(a.data(), b.data(), equals);
+}
+
+template<size_t n>
+bool operator!=(const mat<n>& a, const mat<n>& b)
+{
+    int i = 0;
+    for (auto val_a : a.data())
+    {
+        auto val_b = b.data()[i];
+        if (equals(val_a, val_b)== false)
+        {
+            return true;
+        }
+        i++;
+    }
+    return false;
+}
+
+template<size_t n>
+mat<n> operator*(const mat<n>& a, const mat<n>& b)
+{
+    mat<n> new_mat;
+    
+    for (int i = 0; i != n; i++)
+    {
+        for (int j = 0; j != n; j++)
+        {
+            for (int k = 0; k != n; k++)
+            {
+                new_mat[i][j] += (a[i][k] * b[k][j]);
+            }
+        }
+    }
+
+    return new_mat;
+}
+
+
 using mat4 = mat<4>;
 using mat3 = mat<3>;
 using mat2 = mat<2>;
+
+
+tuple operator*(const mat4& m, const tuple& t)
+{
+    return {
+        m[0][0]*t.x+m[0][1]*t.y+m[0][2]*t.z+m[0][3]*t.w,
+        m[1][0]*t.x+m[1][1]*t.y+m[1][2]*t.z+m[1][3]*t.w,
+        m[2][0]*t.x+m[2][1]*t.y+m[2][2]*t.z+m[2][3]*t.w,
+        m[3][0]*t.x+m[3][1]*t.y+m[3][2]*t.z+m[3][3]*t.w        
+    };
+}
+
--- tests/mat_test.cpp
+++ tests/mat_test.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 
 #include "mat.h"
+#include "tuple.h"
 
 TEST(mat4, At) 
 {
@@ -48,3 +49,107 @@ TEST(mat3, At)
     EXPECT_EQ(mat[1][1], -2);        
     EXPECT_EQ(mat[2][2], 1);        
 }
+
+
+TEST(mat4, Equals) 
+{
+    mat4 mat_a({
+                   1.f,   2.f,   3.f,   4.f,
+                   5.f,  6.f,  7.f,  8.f,
+                   9.f,   8.f,  7.f,  6.f,
+                   5.f, 4.f, 3.f, 2.f
+                });
+
+    mat4 mat_b({
+                   1.f,   2.f,   3.f,   4.f,
+                   5.f,  6.f,  7.f,  8.f,
+                   9.f,   8.f,  7.f,  6.f,
+                   5.f, 4.f, 3.f, 2.f
+                });
+
+    EXPECT_EQ(mat_a, mat_b);    
+    EXPECT_FALSE(mat_a != mat_b);
+}
+
+TEST(mat4, NotEquals) 
+{
+    mat4 mat_a({
+                   1.f,   2.f,   3.f,   4.f,
+                   5.f,  6.f,  7.f,  8.f,
+                   9.f,   8.f,  7.f,  6.f,
+                   5.f, 4.f, 3.f, 2.f
+                });
+
+    mat4 mat_b({
+                   2.f,   3.f,   4.f,   5.f,
+                   6.f,  7.f,  8.f,  9.f,
+                   8.f,   7.f,  6.f,  5.f,
+                   4.f, 3.f, 2.f, 1.f
+                });
+
+    EXPECT_NE(mat_a, mat_b);    
+    EXPECT_TRUE(mat_a != mat_b);
+}
+
+TEST(mat4, Multiplication) 
+{
+    mat4 mat_a({
+                   1.f,   2.f,   3.f,   4.f,
+                   5.f,  6.f,  7.f,  8.f,
+                   9.f,   8.f,  7.f,  6.f,
+                   5.f, 4.f, 3.f, 2.f
+                });
+
+    mat4 mat_b({
+                   -2.f,   1.f,   2.f,   3.f,
+                   3.f,  2.f,  1.f,  -1.f,
+                   4.f,   3.f,  6.f,  5.f,
+                   1.f, 2.f, 7.f, 8.f
+                });
+
+    mat4 result({
+                   20.f,  22.f,   50.f,   48.f,
+                   44.f,  54.f,  114.f,  108.f,
+                   40.f,   58.f,  110.f,  102.f,
+                   16.f, 26.f, 46.f, 42.f
+                });
+
+    EXPECT_EQ(mat_a*mat_b, result);    
+}
+
+TEST(mat4, MultiplicationTuple) 
+{
+    mat4 m({
+                   1.f,   2.f,   3.f,   4.f,
+                   2.f,  4.f,  4.f,  2.f,
+                   8.f,   6.f,  4.f,  1.f,
+                   0.f,   0.f,  0.f   ,1.f
+                });
+
+    tuple t{1, 2, 3, 1};
+
+    tuple expected{18, 24, 33, 1};
+
+    EXPECT_EQ(m*t, expected);    
+}
+
+TEST(mat4, MultiplicationIdentity) 
+{
+    mat4 m({
+                1.f,   2.f,   3.f,   4.f,
+                5.5f,  6.5f,  7.5f,  8.5f,
+                9.f,   10.f,  11.f,  12.f,
+                13.5f, 14.5f, 15.5f, 16.5f
+            });
+                
+    EXPECT_EQ(m*mat4::identity(), m);    
+}
+
+TEST(mat4, MultiplicationIdentityTuple) 
+{
+    tuple t{1, 2, 3, 4};
+
+    EXPECT_EQ(mat4::identity()*t, t);    
+}
+
+
